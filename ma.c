12e32c6de00cf4680f493282fde11827b852a1ee
soniacosta <niniccosta@gmail.com>
2019-04-20 14:45:20 +0100

    v1 com comentarios

--- ma.c
+++ ma.c
@@ -17,16 +17,18 @@ ssize_t readln(int fildes, void *buf, size_t nbyte);
 int main(int argc, char argv[]){
 
     //vars
-    size_t tamLinhaArtigos = 25; //calculos feitos para 25caracteres
-    size_t lidos;
-    char* buffRead = malloc(N);
-    char prog;
-    char* nome;
+    size_t tamLinhaArtigos = 25; //calculos feitos para 24caracteres (25 é para ter espaço para o \0)
+    
+    char* buffRead = malloc(N); //o buff dado ao read
+    size_t lidos;   //return do read(quantos caracteres leu)
     char* palavras[32]; //o maximo de palavras lidas é 32 por convençao
-    int numPalavrasInput = 0;
-    char linha[tamLinhaArtigos];
-    int wcArtigos;
-    int wcStrings;
+    int numPalavrasInput = 0; 
+    char prog;  //opcao : i , n , p
+
+    char* nome; //nome do artigo
+    int tamNome = 0; 
+    
+    char linha[tamLinhaArtigos]; //linha que vai ser escrita no artigos, inicializada a 0
     for(int i = 0; i<tamLinhaArtigos-1;i++){linha[i]='0'; linha[tamLinhaArtigos-1] = '\0';}
     
 
@@ -46,22 +48,23 @@ int main(int argc, char argv[]){
         close(fd[0]); //fechamos o 0 (leitura)
         dup2(fd[1],1);
         close(fd[1]);
-        execlp("wc","wc", "-l", "artigos.txt",(char*)0);
+        execlp("wc","wc", "-l", "artigos.txt",(char*)0); //queremos saber quantas linhas o ficheiro tem
         _exit(0);
     }
 
     close(fd[1]);//0 e de leitura
     char buf[10];
     
-    int n = read(fd[0],buf, sizeof(buf));
+    int n = read(fd[0],buf, sizeof(buf)); //lê do pipe para o buf
     if(n <=0){
         perror(0);
         _exit(errno);
     }
     close(fd[0]);
 
-    sscanf(buf, "%d",&wcArtigos);
-    int numOff_set = wcArtigos*24;
+    int wcArtigos = 0;
+    sscanf(buf, "%d",&wcArtigos); //do buf queremos só a primeira parte (ver wc -l)
+    int numOff_set = wcArtigos*(tamLinhaArtigos-1); //o offset vai ser o numero de linhas * numero de caracteres de uma linha
     lseek(fdArtigos,numOff_set,SEEK_SET);
     /************************************/
     int fd2[2];
@@ -70,21 +73,22 @@ int main(int argc, char argv[]){
         close(fd2[0]); //fechamos o 0 (leitura)
         dup2(fd2[1],1);
         close(fd2[1]);
-        execlp("wc","wc", "-c", "strings.txt",(char*)0);
+        execlp("wc","wc", "-c", "strings.txt",(char*)0); //queremos saber quantos bits existem no ficheiro
         _exit(0);
     }
 
     close(fd2[1]);//0 e de leitura
     char buf2[10];
     
-    int n2 = read(fd2[0],buf2, sizeof(buf2));
+    int n2 = read(fd2[0],buf2, sizeof(buf2)); //lê do pipe para o buf2
     if(n2 <=0){
         perror(0);
         _exit(errno);
     }
     close(fd2[0]);
 
-    sscanf(buf2, "%d",&wcStrings);
+    int wcStrings = 0;
+    sscanf(buf2, "%d",&wcStrings); //do buf2 queremos só a primeira parte (ver wc -c)
     int numOff_setStrings = wcStrings;
     lseek(fdStrings,numOff_setStrings,SEEK_SET);
 
@@ -97,21 +101,21 @@ int main(int argc, char argv[]){
         prog = buffRead[0];
         //printf("%c\n",prog);
         
-        nome = malloc(lidos * sizeof(char));
-        int tam = vectorToString(palavras, nome, 1, numPalavrasInput-2);
         
         
         switch (prog){
             case 'i'://escrever artigo
-                //printf("o artigo %s tem o preco %s \n", palavras[1], palavras[numPalavrasInput-1]);
+                nome = malloc(lidos * sizeof(char));
+                tamNome = vectorToString(palavras, nome, 1, numPalavrasInput-2);
                 sprintf(linha,"%7d %7s %7d\n", wcArtigos, palavras[numPalavrasInput-1], numOff_setStrings);
                 printf("%s \n",nome);
-                write(fdStrings,nome,tam);
+                write(fdStrings,nome,tamNome);
                 write(fdArtigos,linha,tamLinhaArtigos-1);
                 wcArtigos++;
-                numOff_setStrings+=tam;
+                numOff_setStrings+=tamNome;
                 break;
             case 'n':
+
                 break;
             case 'p':
                 break;
